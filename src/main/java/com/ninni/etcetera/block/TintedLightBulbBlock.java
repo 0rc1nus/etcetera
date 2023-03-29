28b88bfc68eaed574bc4134f59653cb0e724b618
0rc1nus <mitchellctlui@gmail.com>
2023-03-29 09:02:11 +0800

    Added Tinted light bulb flickering mechanic

--- src/main/java/com/ninni/etcetera/block/TintedLightBulbBlock.java
+++ src/main/java/com/ninni/etcetera/block/TintedLightBulbBlock.java
@@ -1,8 +1,26 @@
 package com.ninni.etcetera.block;
 
+import com.ninni.etcetera.block.entity.EtceteraBlockEntityType;
+import com.ninni.etcetera.block.entity.TintedLightBulbBlockEntity;
 import com.ninni.etcetera.block.enums.LightBulbBrightness;
+import com.ninni.etcetera.sound.EtceteraSoundEvents;
+import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
-public class TintedLightBulbBlock extends AbstractLightBulbBlock {
+import net.minecraft.block.SculkSensorBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class TintedLightBulbBlock extends AbstractLightBulbBlock implements BlockEntityProvider {
 
     public TintedLightBulbBlock(Settings settings) {
         super(settings.luminance(TintedLightBulbBlock::getLuminance));
@@ -16,4 +34,32 @@ public class TintedLightBulbBlock extends AbstractLightBulbBlock {
         else if (brightness == LightBulbBrightness.BRIGHT) return 10;
         return 0;
     }
+
+    @Override
+    public void turnBrightness(BlockState state, World world, BlockPos pos, LightBulbBrightness brightness) {
+        super.turnBrightness(state, world, pos, brightness);
+        if (world.getBlockEntity(pos) instanceof TintedLightBulbBlockEntity tintedLightBulbBlockEntity && state.get(BRIGHTNESS) == LightBulbBrightness.OFF) {
+            tintedLightBulbBlockEntity.setTicksBeforeFlicker(this.getTicksBeforeFlicker(world));
+        }
+    }
+
+    public int getTicksBeforeFlicker(World world) {
+        return UniformIntProvider.create(200, 1000).get(world.getRandom());
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new TintedLightBulbBlockEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world2, BlockState state, BlockEntityType<T> type) {
+        return !world2.isClient && EtceteraBlockEntityType.TINTED_LIGHT_BULB == type ? (world, pos, state1, blockEntity) -> {
+            TintedLightBulbBlockEntity tintedLightBulbBlockEntity = (TintedLightBulbBlockEntity) blockEntity;
+            tintedLightBulbBlockEntity.tick(world, pos, state1, tintedLightBulbBlockEntity);
+        } : null;
+    }
+
 }
--- src/main/java/com/ninni/etcetera/block/entity/EtceteraBlockEntityType.java
+++ src/main/java/com/ninni/etcetera/block/entity/EtceteraBlockEntityType.java
@@ -3,6 +3,7 @@ package com.ninni.etcetera.block.entity;
 import com.ninni.etcetera.Etcetera;
 import com.ninni.etcetera.block.EtceteraBlocks;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -12,6 +13,12 @@ public class EtceteraBlockEntityType {
             Registry.BLOCK_ENTITY_TYPE, new Identifier(Etcetera.MOD_ID, "item_stand"),
             FabricBlockEntityTypeBuilder.create(ItemStandBlockEntity::new,
                     EtceteraBlocks.ITEM_STAND
+            ).build(null)
+    );
+    public static final BlockEntityType<TintedLightBulbBlockEntity> TINTED_LIGHT_BULB = Registry.register(
+            Registry.BLOCK_ENTITY_TYPE, new Identifier(Etcetera.MOD_ID, "tinted_light_bulb"),
+            FabricBlockEntityTypeBuilder.create(TintedLightBulbBlockEntity::new,
+                    EtceteraBlocks.TINTED_LIGHT_BULB
             ).build(null)
     );
 }
